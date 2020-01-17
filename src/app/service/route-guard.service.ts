7b67612008456c18c8e5d3d8b18b4117bee77904
ABIMAEL RODRIGUES SERGIO <abima@uninove.edu.br>
2020-01-16 22:55:50 -0300

    atualizacao

--- src/app/service/route-guard.service.ts
+++ src/app/service/route-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
 @Injectable({
@@ -8,12 +8,19 @@ import { HardcodedAuthenticationService } from '../service/hardcoded-authenticat
 
 export class RouteGuardService implements CanActivate {
 
-  constructor(private hardcodedAuthenticationService: HardcodedAuthenticationService) { }
+  constructor(
+    private hardcodedAuthenticationService: HardcodedAuthenticationService,
+    private router: Router
+  ) {
+
+  }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
 
     if (this.hardcodedAuthenticationService.isUserLoggedIn())
       return true;
+
+    this.router.navigate(['login']);
 
     return false;
   }
